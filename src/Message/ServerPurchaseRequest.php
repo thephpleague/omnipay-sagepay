19b92638cf223d9d69322519eff4d30faf60d350
James Willans <kyoushu@gmail.com>
2015-08-17 11:46:29 +0100

    Added support for SurchargeXML purchase parameter

--- src/Message/ServerPurchaseRequest.php
+++ src/Message/ServerPurchaseRequest.php
@@ -8,4 +8,28 @@ namespace Omnipay\SagePay\Message;
 class ServerPurchaseRequest extends ServerAuthorizeRequest
 {
     protected $action = 'PAYMENT';
+
+    public function setSurchargeXml($surchargeXml)
+    {
+        $this->setParameter('SurchargeXML', $surchargeXml);
+    }
+
+    public function getSurchargeXml()
+    {
+        return $this->getParameter('SurchargeXML');
+    }
+
+    public function getData()
+    {
+        $data = parent::getData();
+
+        $surchargeXml = $this->getSurchargeXml();
+        if($surchargeXml){
+            $data['SurchargeXML'] = $this->getSurchargeXml();
+        }
+
+        return $data;
+    }
+
+
 }
